66be69165b5dcb20064a4c37aeaa6afc636015c2
eddiepyang <eddiepyang@gmail.com>
2018-12-25 11:07:37 -0500

    adjusted chart format

--- src/visualization/multi_column.py
+++ src/visualization/multi_column.py
@@ -4,12 +4,14 @@ import dash_html_components as html
 import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
-
+import plotly.figure_factory as ff
 
 final = pd.read_hdf('../../data/out/final.h5', 
                     parse_dates = ['date', 'visitStartTime'], 
                     dtype = {'fullvisitorId':int, 'transactionRevenue':int}
             )
+final['log_transaction'] = np.log1p(final.transactionRevenue.astype(float))
+
 
 # data variables
 channel_x = final.channelGrouping.value_counts(normalize=True).index
@@ -32,7 +34,7 @@ html.Div(#style = {'backgroundColor':'#D3D3D3'},
     children = [html.Div([
             #html.H3('Google store charts'),
             dcc.Graph(
-                id='example-graph1',
+                id='g1',
                 figure={
                     'data': [go.Bar(
                             x=channel_x,
@@ -47,9 +49,9 @@ html.Div(#style = {'backgroundColor':'#D3D3D3'},
                             ),
                             opacity=0.6
                             )],
-                    # 'layout': {
-                    #     'title': 'Dash Data Visualization1',
-                    #     #'height': 800,
+                    'layout': {
+                        'title': 'Dash Data Visualization1'}
+                        #'height': 800,
                     #     #'width': 1200
                     #     #'margin': dict(l=10, r=10, t=0, b=0)
                     #             }
@@ -61,14 +63,14 @@ html.Div(#style = {'backgroundColor':'#D3D3D3'},
                 'padding': 1},
                 config={'displayModeBar': False}
                 )
-    ], className="five columns offset-by-halfed chart_div"),
+    ], className="five columns offset-by-one chart_div"),
    
         html.Div(#style = {'backgroundColor':'#D3D3D3'}, 
         children = [
             #html.H3('Column 2'),
             # dcc.Graph(id='g2', figure={'data': [{'y': [1, 2, 3]}]}),
             dcc.Graph(
-        id='example-graph',
+        id='g2',
         figure={
             'data': [
                 # {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
@@ -90,8 +92,10 @@ html.Div(#style = {'backgroundColor':'#D3D3D3'},
     )
         ], className="five columns chart_div"),
     ], className="row"),
-    
-        html.Div([html.H4('Line chart'), dcc.Graph(id='g3', 
+    html.Div([
+
+        html.Div(#[html.H4('Line chart'), 
+        [dcc.Graph(id='g3', 
         figure={'data': [{'y': [1, 2, 3]}]},
         style={"height": "95%", "width": "95%", 
             'display': 'inline-block', 
@@ -99,8 +103,29 @@ html.Div(#style = {'backgroundColor':'#D3D3D3'},
             'padding':1},
         config={'displayModeBar': False}
                 )], 
-        className="five columns offset-by-halfed chart_div ")
-        
+        className="five columns offset-by-one chart_div"),
+
+        html.Div(#[html.H4('Line chart'), 
+        [dcc.Graph(id='g4', 
+        figure = {'data': [go.Scatter(
+    x = final.visitNumber.dropna(0).sample(1000, replace=True),
+    y = final.log_transaction.dropna(0).sample(1000, replace=True),
+    mode = 'markers'
+        )],
+        'layout': go.Layout(
+            xaxis=dict(
+                    range = [0,30]
+                    )
+        )},
+                style={"height": "95%", "width": "95%", 
+                    'padding':1,
+                    'display': 'inline-block'},
+                config={'displayModeBar': False}
+                        )], 
+                className="five columns chart_div"),
+            ], className='row')
+
+           
 ])
 
 if __name__ == '__main__':
